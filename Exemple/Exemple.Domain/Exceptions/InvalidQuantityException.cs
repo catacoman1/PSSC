52558acd25e6394ad7bb1d9855b887341e2c16fc
Serban Sulea <serban.sulea@student.upt.ro>
2023-12-02 01:13:59 +0200

    added new exceptions for the models

---
+++ Exemple/Exemple.Domain/Exceptions/InvalidQuantityException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+namespace Exemple.Domain.Exceptions
+{
+    [Serializable]
+    internal class InvalidQuantityException : Exception
+    {
+        public InvalidQuantityException()
+        {
+        }
+
+        public InvalidQuantityException(string message) : base(message)
+        {
+        }
+
+        public InvalidQuantityException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidQuantityException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
