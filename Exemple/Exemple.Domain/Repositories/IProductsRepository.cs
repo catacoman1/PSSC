ebfdf49b97e32095560429dbea6a85ccb9ee3e03
catacoman1 <ytbcata@gmail.com>
2023-12-02 01:18:53 +0200

    added repositories and product order operationadded productorder operation and repositories for orders and products

---
+++ Exemple/Exemple.Domain/Repositories/IProductsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Exemple.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple.Domain.Repositories
+{
+    public interface IProductsRepository
+    {
+        Task<List<ProductCode>> TryGetExistingProductCodes();
+
+        Task<Quantity> TryGetQuantityForProduct(ProductCode code);
+
+        Task<Price> TryGetPrice(ProductCode code);
+    }
+}
