c899250382e388234e3144edae120b388ec94d97
Serban Sulea <serban.sulea@student.upt.ro>
2023-12-02 01:53:12 +0200

    implemented workflow for place order

---
+++ Exemple/Exemple.Domain/Commands/PlaceOrderCommand.cs
@@ -0,0 +1,19 @@
+﻿using Exemple.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple.Domain.Commands
+{
+    public record PlaceOrderCommand
+    {
+        public PlaceOrderCommand(UnvalidatedOrder inputOrder)
+        {
+            InputOrder = inputOrder;
+        }
+
+        public UnvalidatedOrder InputOrder { get; }
+    }
+}
