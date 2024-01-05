ad76d54fab627cf2315ed8f1b2dcbf6885262f67
catacoman1 <ytbcata@gmail.com>
2024-01-06 01:15:27 +0200

    db context update in order to match new dtos

--- Exemple/Example.Data/ProductsContext.cs
+++ Exemple/Example.Data/ProductsContext.cs
@@ -3,23 +3,46 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Example.Data
 {
-    public class ProductsContext : DbContext
+    public class CartAppDbContext : DbContext
     {
-        public ProductsContext(DbContextOptions<ProductsContext> options) : base(options)
+        public CartAppDbContext()
+        {
+        }
+        public CartAppDbContext(DbContextOptions<CartAppDbContext> options) : base(options)
         {
         }
 
         public DbSet<ProductDto> Products { get; set; }
 
-        public DbSet<OrderLineDto> Orders { get; set; }
+        public DbSet<OrderHeaderDto> OrderHeaders { get; set; }
 
-        public DbSet<OrderHeaderDto> Headers { get; set; }
+        public DbSet<OrderLineDto> OrderLines { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<ProductDto>().ToTable("Product").HasKey(s => s.ProductId);
-            modelBuilder.Entity<OrderHeaderDto>().ToTable("OrderHeader").HasKey(s => s.OrderId);
-            modelBuilder.Entity<OrderLineDto>().ToTable("OrderLine").HasKey(s => s.OrderLineId);
+            modelBuilder.Entity<ProductDto>().ToTable("Products").HasKey(s => s.ProductId);
+            modelBuilder.Entity<OrderHeaderDto>().ToTable("OrderHeaders").HasKey(s => s.OrderId);
+            modelBuilder.Entity<OrderLineDto>().ToTable("OrderLines").HasKey(s => s.OrderLineId);
+
+            modelBuilder.Entity<OrderHeaderDto>()
+                .HasMany(e => e.OrderLines)
+                .WithOne(e => e.OrderHeader)
+                .HasForeignKey(e => e.OrderId)
+                .HasPrincipalKey(e => e.OrderId);
+
+            modelBuilder.Entity<ProductDto>()
+                .HasMany(e => e.OrderLines)
+                .WithOne(e => e.Product)
+                .HasForeignKey(e => e.ProductId)
+                .HasPrincipalKey(e => e.ProductId);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=Magazin1;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
         }
 
     }
